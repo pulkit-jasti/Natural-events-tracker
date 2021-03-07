4e4e7af9854e2ac78d42bf6f1e03e01f317b5a1b
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-08 04:38:33 +0530

    Leaflet map working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 function App() {
-	return <div>Yup, it's working</div>;
+	return (
+		<div>
+			Yup, it's workkkkkking
+			<MapContainer style={{ height: '80vh' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+				<Marker position={[51.505, -0.09]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
+		</div>
+	);
 }
 
 export default App;
